b3b160d699c8a6a0fdc692a480e9104788f3f8cd
TARUN BADDULA <TARUN BADDULA@Nagas>
2022-08-09 11:08:15 +0530

    First commit

--- JavaSupport/src/main/java/com/java/support/JavaSupport/JavaSupportApplication.java
+++ JavaSupport/src/main/java/com/java/support/JavaSupport/JavaSupportApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaSupportApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaSupportApplication.class, args);
+		SpringApplication.run(JavaSupportApplication.class, args); 
 	}
 
 }
